ea339ec02ec89e2432d455e3ad344c138774fbf9
bender <francesco.altiero86@gmail.com>
2019-03-06 14:16:35 +0100

    Updates

---
+++ precache-manifest.db53b6398c2c3d801d0e82abd9a98628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8acf68b37047949db0c",
+    "url": "/amasingapp/static/js/runtime~main.c8acf68b.js"
+  },
+  {
+    "revision": "df9016e9884faf40f568",
+    "url": "/amasingapp/static/js/main.df9016e9.chunk.js"
+  },
+  {
+    "revision": "e09c54457d56727de06a",
+    "url": "/amasingapp/static/js/2.e09c5445.chunk.js"
+  },
+  {
+    "revision": "df9016e9884faf40f568",
+    "url": "/amasingapp/static/css/main.fafa5bf5.chunk.css"
+  },
+  {
+    "revision": "e09c54457d56727de06a",
+    "url": "/amasingapp/static/css/2.88335501.chunk.css"
+  },
+  {
+    "revision": "2fb41e1b07cc6da022997f531f97c677",
+    "url": "/amasingapp/index.html"
+  }
+];
