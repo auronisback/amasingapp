28e681120afa256812611ae5991c06569f528577
bender <francesco.altiero86@gmail.com>
2019-04-10 16:22:24 +0200

    Fixed package.json in order to refer to rootDir in test results

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, {shallow} from 'enzyme';
 import App from '../App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Configuring adapter
+Enzyme.configure({adapter: new Adapter()});
+
+describe('<App>', () => {
+  it('Renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('Should show the marker if a destination is clicked', () => {
+    const app = shallow(<App />).instance();
+    app.onDestinationClick();
+    expect(app).toMatchSnapshot();
+  });
+
+  it('Should close the search window', () => {
+    const app = shallow(<App />).instance();
+    app.onSearchWindowClosed();
+    expect(app).toMatchSnapshot();
+  });
+
+  it('Should set or unset the destination', () => {
+    const app = shallow(<App />).instance();
+    app.setDestination({lat: 12, lng: 13});
+    expect(app).toMatchSnapshot();
+    app.setDestination(null);
+    expect(app).toMatchSnapshot();
+  });
 });
